81dbdb0dc8ba664896d075c1e2ac8643a5b41c32
Volker Killesreiter <killesre@gmail.com>
2017-02-02 10:30:48 +0100

    check for grid

--- js/isotope_views.js
+++ js/isotope_views.js
@@ -16,59 +16,61 @@
             columnWidth: '.grid-sizer'
           }
         });
+//        .once('isotope-processed')
 
-      //****************************
-      // Isotope Load more button
-      //****************************
-      var initShow = 4; //number of items loaded on init & onclick load more button
-      var counter = initShow; //counter for load more button
-      var isotope = $grid.data('isotope'); // get Isotope instance
+      if ($grid.length) {
+        //****************************
+        // Isotope Load more button
+        //****************************
+        var initShow = 4; //number of items loaded on init & onclick load more button
+        var counter = initShow; //counter for load more button
+        var isotope = $grid.data('isotope'); // get Isotope instance
 
-      loadMore(initShow); //execute function onload
+        loadMore(initShow); //execute function onload
 
 
-      function loadMore(toShow) {
+        function loadMore(toShow) {
 
-        var shownElems = isotope.filteredItems.slice(0, toShow).map(function(item) {
-          return item.element;
-        });
-        $(shownElems).removeClass('hidden');
-        $grid.isotope('layout');
+          var shownElems = isotope.filteredItems.slice(0, toShow).map(function (item) {
+            return item.element;
+          });
+          $(shownElems).removeClass('hidden');
+          $grid.isotope('layout');
 
-        //when no more to load, hide show more button
-        if ($grid.find(".hidden").length == 0) {
-          $("#isotope-load-more").hide();
+          //when no more to load, hide show more button
+          if ($grid.find(".hidden").length == 0) {
+            $("#isotope-load-more").hide();
+          }
         };
 
-      }
+        //append load more button
+        $grid.after('<button type="button" class="btn btn-default btn-lg" id="isotope-load-more"> Load More</button>');
 
-      //append load more button
-      $grid.after('<button type="button" class="btn btn-default btn-lg" id="isotope-load-more"> Load More</button>');
+        //when load more button clicked
+        $("#isotope-load-more").click(function () {
+          counter = counter + initShow;
 
-      //when load more button clicked
-      $("#isotope-load-more").click(function() {
-        counter = counter + initShow;
-
-        loadMore(counter);
-        $grid.isotope('layout');
-      });
-
-      var timer;
-
-      // Repaint layout
-      var layout = function() {
-        if (timer) {
-          clearTimeout(timer);
-        }
-        // iOS Safari requires 72msec delay, why?
-        timer = setTimeout(function() {
+          loadMore(counter);
           $grid.isotope('layout');
-        }, 72);
-      };
+        });
 
-      // Attach to imageloaded and flexslider events
-      $grid.imagesLoaded().progress(layout);
-      // $grid.on('flexslider-init', layout);
+        var timer;
+
+        // Repaint layout
+        var layout = function () {
+          if (timer) {
+            clearTimeout(timer);
+          }
+          // iOS Safari requires 72msec delay, why?
+          timer = setTimeout(function () {
+            $grid.isotope('layout');
+          }, 72);
+        };
+
+        // Attach to imageloaded and flexslider events
+        $grid.imagesLoaded().progress(layout);
+        // $grid.on('flexslider-init', layout);
+      }
     }
   };
 
