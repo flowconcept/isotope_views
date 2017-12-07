2862fac46a2a4afc08362e3496c1801950420a26
Tobias Maerz <t.maerz@flowconcept.de>
2017-12-07 15:17:00 +0100

    =Replaced loadMore function

--- js/isotope_views.js
+++ js/isotope_views.js
@@ -1,92 +1,87 @@
 (function($, Drupal) {
-  "use strict";
+    "use strict";
 
-  /**
-   * Isotope layout.
-   */
-  Drupal.behaviors.isotope = {
-    attach: function(context, settings) {
-      $('.isotope-grid', context)
-        .not('.isotope-attached')
-        .addClass('isotope-attached')
-        .each(function () {
+    /**
+     * Isotope layout.
+     */
+    Drupal.behaviors.isotope = {
+        attach: function(context, settings) {
+            $('.isotope-grid', context)
+                .not('.isotope-attached')
+                .addClass('isotope-attached')
+                .each(function () {
 
-          var $grid = $(this),
-            settings = $(this).data('isotopesettings');
+                    var $grid = $(this),
+                        settings = $(this).data('isotopesettings');
 
-          $grid.isotope({
-            itemSelector: '.grid-item',
-            percentPosition: true,
-            layoutMode: 'masonry',
-            masonry: {
-              columnWidth: '.grid-sizer'
-            }
-          });
+                    $grid.isotope({
+                        itemSelector: '.grid-item',
+                        percentPosition: true,
+                        layoutMode: 'masonry',
+                        masonry: {
+                            columnWidth: '.grid-sizer'
+                        }
+                    });
 
 
-          if ($grid.length && settings.use_load_more) {
-            //****************************
-            // Isotope Load more button
-            //****************************
-            var initShow = parseInt(settings.initial_number_of_items); //number of items loaded on init & onclick load more button
-            var counter = initShow; //counter for load more button
-            var isotope = $grid.data('isotope'); // get Isotope instance
+                    if ($grid.length && settings.use_load_more) {
+                        //****************************
+                        // Isotope Load more button
+                        //****************************
+                        var initShow = parseInt(settings.initial_number_of_items); //number of items loaded on init & onclick load more button
+                        var counter = initShow; //counter for load more button
+                        var isotope = $grid.data('isotope'); // get Isotope instance
 
-            loadMore(initShow); //execute function onload
+                        showMoreItems(initShow); //execute function onload
 
-            if (initShow < isotope.items.length) {
-              //append load more button
-              $grid.after('<div class="col-xs-12 text-center"><button type="button" class="btn btn-default btn-lg isotope-load-more">' + Drupal.t('Load More') + '</button></div>');
+                        if (initShow < isotope.items.length) {
+                            //append load more button
+                            $grid.after('<div class="col-xs-12 text-center"><button type="button" class="btn btn-default btn-lg isotope-load-more">' + Drupal.t('Load More') + '</button></div>');
 
-              //when load more button clicked
-              $(".isotope-load-more", context).on('click', function () {
-                var parent = $(this).parents('.view-gallery-images');
+                            //when load more button clicked
+                            $(".isotope-load-more", context).on('click', function () {
+                                var parent = $(this).parents('.view-gallery-images');
 
-                showMoreItems(parent);
-                // loadMore(counter);
-                $grid.isotope('layout');
-              });
-            }
+                                showMoreItems(parent);
+                                // loadMore(counter);
+                                $grid.isotope('layout');
+                            });
+                        }
 
-          }
+                    }
 
-          function showMoreItems(parent) {
-             parent.find('.hidden').removeClass('hidden');
-             parent.find('.isotope-load-more').hide();
-          }
+                    function showMoreItems(parent) {
+                        if (isNaN(parent)) {
+                            parent.find('.hidden').removeClass('hidden');
+                            parent.find('.isotope-load-more').hide();
+                        } else {
+                            var shownElems = isotope.filteredItems.slice(0, parent).map(function (item) {
+                                return item.element;
+                            });
+                            $(shownElems).removeClass('hidden');
+                            $grid.isotope('layout');
+                        }
+                    }
 
-          function loadMore(toShow) {
+                    var timer;
 
-            var shownElems = isotope.filteredItems.slice(0, toShow).map(function (item) {
-              return item.element;
-            });
-            $(shownElems).removeClass('hidden');
-            $grid.isotope('layout');
+                    // Repaint layout
+                    var layout = function (e) {
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+                        // iOS Safari requires 72msec delay, why?
+                        timer = setTimeout(function () {
+                            $grid.isotope('layout');
+                        }, 72);
+                    };
 
-            //when no more to load, hide show more button
-            if ($grid.find(".hidden").length == 0) {
-              $(".isotope-load-more").hide();
-            }
-          };
-
-          var timer;
-
-          // Repaint layout
-          var layout = function (e) {
-            if (timer) {
-              clearTimeout(timer);
-            }
-            // iOS Safari requires 72msec delay, why?
-            timer = setTimeout(function () {
-              $grid.isotope('layout');
-            }, 72);
-          };
-
-          // Attach to lazloader and flexslider events
-          $grid.on('lazybeforeunveil', layout);
-          $grid.on('flexslider-init', layout);
-        });
-    }
-  };
+                    // Attach to lazloader and flexslider events
+                    $grid.on('lazybeforeunveil', layout);
+                    $grid.on('flexslider-init', layout);
+                });
+        }
+    };
 
 })(jQuery, Drupal);
+
